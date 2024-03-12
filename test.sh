2e2de37a7d5726c096cdd81df03d04b2c07145ba
Fabricio <fabricio.zuardi@luizalabs.com>
2024-03-12 13:18:21 -0300

    main script, to fill shellspec env vars and filters

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# arguments
+source ./vendor/yaacov/argparse.sh
+define_arg "clients" "aws-s3api,aws-s3,rclone" "S3 clients that will perform the tests" "string" "false"
+define_arg "profiles" "" "Profiles to use in the tests, must have the same name on all clients" "string" "true"
+define_arg "tests" "" "A list of individual tests to perform" "string" "false"
+set_description "Run tests on multiple S3 providers using multiple S3 clients."
+check_for_help "$@"
+parse_args "$@"
+
+# convert comma separated lists to space separated
+clients="${clients//,/ }"
+profiles="${profiles//,/ }"
+tests="${tests//,/ }"
+
+# convert test numbers to test "id" tags
+tag_args=""
+for num in $tests; do
+    padded_num=$(printf "%03d" $num)  # Padding each number with zeroes to three digits
+    tag_args+=" --tag id:${padded_num}"
+done
+
+shellspec --env CLIENTS="$clients" --env PROFILES="$profiles" $tag_args
