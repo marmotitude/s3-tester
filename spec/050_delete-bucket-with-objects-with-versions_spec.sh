48e9c4c3767618ecbb636d54e5b82ebc40429e06
Bruno Fernandes <bruno.fernandes1996@hotmail.com>
2024-03-20 15:10:16 -0300

    fix test with mgc

--- spec/050_delete-bucket-with-objects-with-versions_spec.sh
+++ spec/050_delete-bucket-with-objects-with-versions_spec.sh
@@ -11,12 +11,12 @@ Describe 'Delete bucket with objects with versions:' category:"Object Versioning
   Example "on profile $1 using client $2" id:"050"
     profile=$1
     client=$2    
-    case "$client" in
-    "aws-s3api" | "aws" | "aws-s3")
     aws --profile $profile s3api create-bucket --bucket $bucket_name-$client| jq
     aws s3api --profile $profile put-bucket-versioning --bucket $bucket_name-$client --versioning-configuration Status=Enabled
     aws --profile $profile s3 cp $file1_name  s3://$bucket_name-$client
     aws --profile $profile s3 cp $file1_name  s3://$bucket_name-$client
+    case "$client" in
+    "aws-s3api" | "aws" | "aws-s3")
     When run aws --profile $profile s3 rb s3://$bucket_name-$client
     The status should be failure
     The stderr should include BucketNotEmpty
@@ -24,10 +24,6 @@ Describe 'Delete bucket with objects with versions:' category:"Object Versioning
     aws --profile $profile s3 rb s3://$bucket_name-$client --force
       ;;
     "rclone")
-    aws --profile $profile s3api create-bucket --bucket $bucket_name-$client| jq
-    aws s3api --profile $profile put-bucket-versioning --bucket $bucket_name-$client --versioning-configuration Status=Enabled
-    aws --profile $profile s3 cp $file1_name  s3://$bucket_name-$client
-    aws --profile $profile s3 cp $file1_name  s3://$bucket_name-$client
     When run rclone delete $profile:$bucket_name-$client
     The status should be success
     The output should include ""
@@ -36,7 +32,13 @@ Describe 'Delete bucket with objects with versions:' category:"Object Versioning
     aws --profile $profile s3 rb s3://$bucket_name-$client --force
       ;;
     "mgc")
-    Skip 'Teste pulado para cliente mgc'
+    When run mgc object-storage buckets delete $bucket_name-$client -f --force
+    The status should be failure
+    The stderr should include BucketNotEmpty
+    The output should include "Deleting"
+    aws --profile $profile s3api delete-objects --bucket $bucket_name-$client --delete "$(aws --profile $profile s3api list-object-versions --bucket $bucket_name-$client| jq '{Objects: [.Versions[] | {Key:.Key, VersionId : .VersionId}], Quiet: false}')" | jq
+    aws --profile $profile s3api delete-objects --bucket $bucket_name-$client --delete "$(aws --profile $profile s3api list-object-versions --bucket $bucket_name-$client| jq '{Objects: [.DeleteMarkers[] | {Key:.Key, VersionId : .VersionId}], Quiet: false}')" | jq
+    aws --profile $profile s3 rb s3://$bucket_name-$client --force
       ;;
     esac
   End
