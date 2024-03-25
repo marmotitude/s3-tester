3ad33eeb4089c176eae6cbc1ed01a5045f69a5ef
Bruno Fernandes <bruno.fernandes1996@hotmail.com>
2024-03-25 11:53:46 -0300

    fix skip

--- spec/045_download-object-to-versioning-in-the-public-bucket_spec.sh
+++ spec/045_download-object-to-versioning-in-the-public-bucket_spec.sh
@@ -19,17 +19,19 @@ Describe 'Download object to versioning in the public bucket:' category:"Object 
     When run aws --profile $profile-second s3api get-object --bucket $bucket_name-$client --key $file1_name $file1_name-2
     The status should be failure
     The stderr should include "An error occurred (AccessDenied) when calling the GetObject operation: Access Denied."
+    aws --profile $profile s3api delete-objects --bucket $bucket_name-$client --delete "$(aws --profile $profile s3api list-object-versions --bucket $bucket_name-$client| jq '{Objects: [.Versions[] | {Key:.Key, VersionId : .VersionId}], Quiet: false}')"  > /dev/null
       ;;
     "rclone")
     When run rclone copy $profile-second:$bucket_name-$client/$file1_name $file1_name-2
     The status should be failure
     The output should include ""
+    aws --profile $profile s3api delete-objects --bucket $bucket_name-$client --delete "$(aws --profile $profile s3api list-object-versions --bucket $bucket_name-$client| jq '{Objects: [.Versions[] | {Key:.Key, VersionId : .VersionId}], Quiet: false}')"  > /dev/null
       ;;
     "mgc")
+    aws --profile $profile s3api delete-objects --bucket $bucket_name-$client --delete "$(aws --profile $profile s3api list-object-versions --bucket $bucket_name-$client| jq '{Objects: [.Versions[] | {Key:.Key, VersionId : .VersionId}], Quiet: false}')"  > /dev/null
     Skip "Skipped test to $client"
       ;;
     esac
-    aws --profile $profile s3api delete-objects --bucket $bucket_name-$client --delete "$(aws --profile $profile s3api list-object-versions --bucket $bucket_name-$client| jq '{Objects: [.Versions[] | {Key:.Key, VersionId : .VersionId}], Quiet: false}')"  > /dev/null
     aws --profile $profile s3 rb s3://$bucket_name-$client --force > /dev/null
     rm -rf $file1_name-2
   End
