a3d597eb815c548b72283df3c14670959995487b
Fabricio <fabricio.zuardi@luizalabs.com>
2024-03-26 20:19:24 -0300

    add mgc to tests 53,57,61,62 (only one profile, the default)

--- spec/053_upload-files_spec.sh
+++ spec/053_upload-files_spec.sh
@@ -65,10 +65,15 @@ Describe 'Upload Files' category:"Object Management"
       The error should include "$local_file: Copied"
       The error should include "to: $key"
       ;;
+    "mgc")
+      When run mgc object-storage objects upload --src="$local_file" --dst="$BUCKET_NAME/$key"
+      The status should be success
+      The output should include "Uploaded file $local_file to $BUCKET_NAME/$keys3"
+      ;;
     esac
   End
   Describe 'Download Files' category:"Object Management" id:"057"
-    Example "from bucket $BUCKET_NAME of profile $1, The file $3, using client $2"
+    Example "from test bucket of profile $1, The file $3, using client $2"
       profile=$1
       client=$2
       file=$3
@@ -93,6 +98,11 @@ Describe 'Upload Files' category:"Object Management"
         The error should include "$object_key: Copied"
         The status should be success
         ;;
+      "mgc")
+        When run mgc object-storage objects download --dst="$out_file" --src="$BUCKET_NAME/$object_key"
+        The status should be success
+        The output should include "Downloaded from $BUCKET_NAME/$object_key to $out_file"
+        ;;
       esac
     End
   End
@@ -109,29 +119,35 @@ Describe 'List Objects' category:"Object Management" id:"061"
     case "$client" in
     "aws-s3api" | "aws")
       When run aws --profile $profile s3api list-objects --bucket $BUCKET_NAME
+      The status should be success
       for file in $FILES;do
         object_key=$(get_uploaded_key "$file")
         The output should include "\"Key\": \"$object_key\","
-        The status should be success
       done
       ;;
     "aws-s3")
       When run aws --profile $profile s3 ls "s3://$BUCKET_NAME"
+      The status should be success
       for file in $FILES;do
         object_key=$(get_uploaded_key "$file")
         The output should include " $object_key"
-        The status should be success
       done
       ;;
     "rclone")
       When run rclone ls $profile:$BUCKET_NAME
+      The status should be success
       for file in $FILES;do
         object_key=$(get_uploaded_key "$file")
         The output should include " $object_key"
-        The status should be success
       done
       ;;
     "mgc")
+      When run mgc object-storage objects list --dst="$BUCKET_NAME"
+      The status should be success
+      for file in $FILES;do
+        object_key=$(get_uploaded_key "$file")
+        The output should include " $object_key"
+      done
       ;;
     esac
   End
@@ -146,7 +162,7 @@ Describe 'Delete' category:"Object Management"
     $CLIENTS
   End
   Describe "Objects"
-  Example "on profile $1, using client $2, delete file $first_file on bucket $BUCKET_NAME" id:"062"
+  Example "on profile $1, using client $2, delete file $first_file on test bucket" id:"062"
     profile=$1
     client=$2
     BUCKET_NAME=$(get_test_bucket_name)
@@ -154,76 +170,100 @@ Describe 'Delete' category:"Object Management"
     case "$client" in
     "aws-s3api" | "aws")
       When run aws --profile "$profile" s3api delete-object --bucket "$BUCKET_NAME" --key "$object_key" --debug
-      The error should include "$object_key HTTP/1.1\" 204"
       The status should be success
+      The error should include "$object_key HTTP/1.1\" 204"
       ;;
     "aws-s3")
       When run aws --profile $profile s3 rm "s3://$BUCKET_NAME/$object_key"
-      The output should include "delete: s3://$BUCKET_NAME/$object_key"
       The status should be success
+      The output should include "delete: s3://$BUCKET_NAME/$object_key"
       ;;
     "rclone")
       When run rclone deletefile "$profile:$BUCKET_NAME/$object_key" -v
-      The error should include "$object_key: Deleted"
       The status should be success
+      The error should include "$object_key: Deleted"
       ;;
     "mgc")
-      :;
+      When run mgc --debug object-storage objects delete --dst="$BUCKET_NAME/$object_key" --cli.bypass-confirmation
+      The status should be success
+      The error should include "$BUCKET_NAME?delete="
+      The error should include "200 OK"
       ;;
     esac
   End
   End
   Describe "Objects in batch"
-  Example "on profile $1, using client $2, batch delete files $remaining_files on bucket $BUCKET_NAME" id:"063"
-    profile=$1
-    client=$2
-    BUCKET_NAME=$(get_test_bucket_name)
-    objects=""
-    for file in $remaining_files; do
-      object_key=$(get_uploaded_key "$file")
-      objects+="$object_key "
-    done
-    case "$client" in
-    "aws-s3api" | "aws")
-      s3api_objects="Objects=["
-      for object_key in $objects; do
-        s3api_objects+="{Key=$object_key},"
+    Example "on profile $1, using client $2, batch delete files $remaining_files on bucket $BUCKET_NAME" id:"063"
+      profile=$1
+      client=$2
+      BUCKET_NAME=$(get_test_bucket_name)
+      objects=""
+      for file in $remaining_files; do
+        object_key=$(get_uploaded_key "$file")
+        objects+="$object_key "
       done
-      s3api_objects+="]"
+      case "$client" in
+      "aws-s3api" | "aws")
+        s3api_objects="Objects=["
+        for object_key in $objects; do
+          s3api_objects+="{Key=$object_key},"
+        done
+        s3api_objects+="]"
 
-      When run aws --profile "$profile" s3api delete-objects --bucket "$BUCKET_NAME" --delete "$s3api_objects"
-      for object_key in $objects; do
-        The status should be success
-        The output should include "\"Key\": \"$object_key\""
-      done
-      ;;
-    "aws-s3")
-      s3_args=''
-      for object_key in $objects; do
-        s3_args+=" --include $object_key"
-      done
+        When run aws --profile "$profile" s3api delete-objects --bucket "$BUCKET_NAME" --delete "$s3api_objects"
+        for object_key in $objects; do
+          The status should be success
+          The output should include "\"Key\": \"$object_key\""
+        done
+        ;;
+      "aws-s3")
+        s3_args=''
+        for object_key in $objects; do
+          s3_args+=" --include $object_key"
+        done
 
-      When run aws --profile $profile s3 rm "s3://$BUCKET_NAME/" --recursive --exclude "*" $s3_args
-      for object_key in $objects; do
-        The status should be success
-        The output should include "delete: s3://$BUCKET_NAME/$object_key"
-      done
-      ;;
-    "rclone")
-      # convert space separated list to comma separated
-      rclone_objects="${objects// /,}"
+        When run aws --profile $profile s3 rm "s3://$BUCKET_NAME/" --recursive --exclude "*" $s3_args
+        for object_key in $objects; do
+          The status should be success
+          The output should include "delete: s3://$BUCKET_NAME/$object_key"
+        done
+        ;;
+      "rclone")
+        # convert space separated list to comma separated
+        rclone_objects="${objects// /,}"
 
-      When run rclone delete "$profile:$BUCKET_NAME" --include "{$rclone_objects}" --dump headers
-      for object_key in $objects; do
+        When run rclone delete "$profile:$BUCKET_NAME" --include "{$rclone_objects}" --dump headers
+        for object_key in $objects; do
+          The status should be success
+          The error should include "$object_key: Deleted"
+        done
+        ;;
+      "mgc")
+        mgc_objects="[{}"
+        for object_key in $objects; do
+          mgc_objects+=',{"include": "'
+          mgc_objects+=$object_key
+          mgc_objects+='"}'
+        done
+        mgc_objects+="]"
+        # TODO: mgc is not properly supporting --include lists, see https://chat.google.com/room/AAAATfofxEQ/HiM7LN_4CUw/HiM7LN_4CUw?cls=10
+        When run mgc object-storage objects delete-all "$BUCKET_NAME" --cli.bypass-confirmation --filter="$mgc_objects"
         The status should be success
-        The error should include "$object_key: Deleted"
+        The output should include "Deleting objects from \"$BUCKET_NAME\""
+        ;;
+      esac
+    End
+    Example "on profile $1, list objects on bucket $BUCKET_NAME DONT include files:$remaining_files" id:"063"
+      profile=$1
+      client=$2
+      BUCKET_NAME=$(get_test_bucket_name)
+      When run aws --profile $profile s3api list-objects --bucket $BUCKET_NAME
+      The status should be success
+      for file in $remaining_files;do
+        object_key=$(get_uploaded_key "$file")
+        The output should not include "\"Key\": \"$object_key\","
       done
-      ;;
-    "mgc")
-      :;
-      ;;
-    esac
-  End
+    End
   End
 End
 
