d40e90391f05d76e8c744d08e26feb54ed2d8b7f
Bruno Fernandes <bruno.fernandes1996@hotmail.com>
2024-04-13 11:59:42 -0300

    adicionado test para put-presign

--- spec/036_validate-the-url-of-presigned_spec.sh
+++ spec/036_validate-the-url-of-presigned_spec.sh
@@ -35,3 +35,37 @@ Describe 'get-presign:' category:"Bucket Sharing"
     aws s3 rb s3://$bucket_name-$client --profile $profile --force > /dev/null
   End
 End
+
+Describe 'put-presign:' category:"Bucket Sharing"
+  setup(){
+    bucket_name="test-036-$(date +%s)"
+    file1_name="LICENSE"
+  }
+  Before 'setup'
+  Parameters:matrix
+    $PROFILES
+    $CLIENTS
+  End
+  Example "on profile $1 using client $2" id:"036"
+    profile=$1
+    client=$2
+    aws --profile $profile s3 mb s3://$bucket_name-$client > /dev/null
+    aws --profile $profile s3 cp $file1_name s3://$bucket_name-$client > /dev/null
+    case "$client" in
+    "aws-s3api" | "aws" | "aws-s3")
+      Skip "Skipped test to $client"
+      ;;
+    "rclone")
+      Skip "Skipped test to $client"
+      ;;
+    "mgc")
+    mgc profile set-current $profile > /dev/null
+    presign_url=$(mgc object-storage objects presign --dst $bucket_name-$client/$file1_name --expires-in "5m" --method PUT)
+    When run curl -X PUT -T $file1_name $presign_url
+    The error should include Current
+      ;;
+    esac
+    The status should be success
+    aws s3 rb s3://$bucket_name-$client --profile $profile --force > /dev/null
+  End
+End
