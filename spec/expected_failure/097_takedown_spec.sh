5461d4c04728b3cc943f62a21e8f56ce9484f305
Bruno Fernandes <bruno.fernandes1996@hotmail.com>
2024-04-16 16:42:29 -0300

    adicionado testes com usuarios bloqueados

---
+++ spec/expected_failure/097_takedown_spec.sh
@@ -0,0 +1,175 @@
+Describe 'Takedown Create bucket:' category:"Bucket Permission"
+  setup(){
+    bucket_name="test-097-$(date +%s)"
+  }
+  Before 'setup' 
+  Parameters:matrix
+    $PROFILES
+    $CLIENTS
+  End
+  Example "on profile $1 using client $2" id:"097"
+    profile=$1
+    client=$2
+    case "$client" in
+    "aws-s3api" | "aws")
+      When run aws --profile $profile s3api create-bucket --bucket $bucket_name-$client
+      The stderr should include "Blocked account"
+      ;;
+    "aws-s3")
+      When run aws --profile $profile s3 mb s3://$bucket_name-$client
+      The stderr should include "Blocked account"
+      ;;
+    "rclone")
+      When run rclone mkdir $profile:$bucket_name-$client -v
+      The stderr should include "Blocked account"
+      ;;
+    "mgc")
+      mgc profile set-current $profile > /dev/null
+      When run mgc object-storage buckets create $bucket_name-$client
+      The stderr should include "Blocked account"
+      ;;
+    esac
+    The status should be failure
+  End
+End
+
+Describe 'Takedown List buckets:' category:"Bucket Permission"
+  setup(){
+    bucket_name="test-097-$(date +%s)"
+  }
+  Before 'setup' 
+  Parameters:matrix
+    $PROFILES
+    $CLIENTS
+  End
+  Example "on profile $1 using client $2" id:"097"
+    profile=$1
+    client=$2
+    case "$client" in
+    "aws-s3api" | "aws")
+      When run aws --profile $profile s3api list-buckets
+      The stderr should include "Blocked account"
+      ;;
+    "aws-s3")
+      When run aws --profile $profile s3 ls
+      The stderr should include "Blocked account"
+      ;;
+    "rclone")
+      When run rclone lsd $profile: -v
+      The stderr should include "Blocked account"
+      ;;
+    "mgc")
+      mgc profile set-current $profile > /dev/null
+      When run mgc object-storage buckets list
+      The stderr should include "Blocked account"
+      ;;
+    esac
+    The status should be failure
+  End
+End
+
+Describe 'Takedown List objects:' category:"Bucket Permission"
+  setup(){
+    bucket_name="test-097-$(date +%s)"
+  }
+  Before 'setup' 
+  Parameters:matrix
+    $PROFILES
+    $CLIENTS
+  End
+  Example "on profile $1 using client $2" id:"097"
+    profile=$1
+    client=$2
+    case "$client" in
+    "aws-s3api" | "aws")
+      When run aws --profile $profile s3api list-objects-v2 --bucket $bucket_name-$client
+      The stderr should include "Blocked account"
+      ;;
+    "aws-s3")
+      When run aws --profile $profile s3 ls s3://$bucket_name-$client
+      The stderr should include "Blocked account"
+      ;;
+    "rclone")
+      When run rclone lsd $profile:$bucket_name-$client -v
+      The stderr should include "Blocked account"
+      ;;
+    "mgc")
+      mgc profile set-current $profile > /dev/null
+      When run mgc object-storage objects list $bucket_name-$client
+      The stderr should include "Blocked account"
+      ;;
+    esac
+    The status should be failure
+  End
+End
+
+Describe 'Takedown Delete object:' category:"Bucket Permission"
+  setup(){
+    bucket_name="test-097-$(date +%s)"
+    file1_name="LICENSE"
+  }
+  Before 'setup' 
+  Parameters:matrix
+    $PROFILES
+    $CLIENTS
+  End
+  Example "on profile $1 using client $2" id:"097"
+    profile=$1
+    client=$2
+    case "$client" in
+    "aws-s3api" | "aws")
+      When run aws --profile $profile s3api delete-object --bucket $bucket_name-$client --key $file1_name
+      The stderr should include "Blocked account"
+      ;;
+    "aws-s3")
+      When run aws --profile $profile s3 rm s3://$bucket_name-$client/$file1_name
+      The stderr should include "Blocked account"
+      ;;
+    "rclone")
+      When run rclone delete $profile:$bucket_name-$client/$file1_name -v
+      The stderr should include "Blocked account"
+      ;;
+    "mgc")
+      mgc profile set-current $profile > /dev/null
+      When run mgc object-storage objects delete --dst $bucket_name-$client/$file1_name -f
+      The stderr should include "Blocked account"
+      ;;
+    esac
+    The status should be failure
+  End
+End
+
+Describe 'Takedown Delete bucket:' category:"Bucket Permission"
+  setup(){
+    bucket_name="test-097-$(date +%s)"
+  }
+  Before 'setup' 
+  Parameters:matrix
+    $PROFILES
+    $CLIENTS
+  End
+  Example "on profile $1 using client $2" id:"097"
+    profile=$1
+    client=$2
+    case "$client" in
+    "aws-s3api" | "aws")
+      When run aws --profile $profile s3api delete-bucket --bucket $bucket_name-$client 
+      The stderr should include "Blocked account"
+      ;;
+    "aws-s3")
+      When run aws --profile $profile s3 rb s3://$bucket_name-$client
+      The stderr should include "Blocked account"
+      ;;
+    "rclone")
+      When run rclone purge $profile:$bucket_name-$client -v
+      The stderr should include "Blocked account"
+      ;;
+    "mgc")
+      mgc profile set-current $profile > /dev/null
+      When run mgc object-storage buckets delete $bucket_name-$client -f
+      The stderr should include "Blocked account"
+      ;;
+    esac
+    The status should be failure
+  End
+End
