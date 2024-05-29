6a048cfb9d6619e21fa48aa4b7fd0736c13351b8
Fabricio <fabricio.zuardi@luizalabs.com>
2024-05-29 15:33:56 -0300

    feat: include awscli presign in the js test to have a comparison

--- spec/js/small.test.ts
+++ spec/js/small.test.ts
@@ -10,15 +10,21 @@ import {
   DeleteObjectCommand,
 } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import util from 'node:util';
+import { exec as nodeExec } from 'node:child_process';
+const exec = util.promisify(nodeExec)
 
 // AWS S3 credentials and region from environment variables
 const { AWS_PROFILE } = process.env;
 // Create S3 client with provided credentials and region
-const client = new S3Client();
+const client = new S3Client({forcePathStyle: true});
 const BUCKET_NAME = `test-${AWS_PROFILE}-${new Date().getTime()}`
 const OBJECT_KEY = "key1"
 const OBJECT_CONTENT = "This is my object's content"
 const EXPIRATION_TIME = 200 // seconds
+
+let signedGetUrlFromSdk
+let signedGetUrlFromCli
 
 describe("Small tests", async () => {
   //setup
@@ -61,21 +67,33 @@ describe("Small tests", async () => {
   });
 
   describe("Presign URLs", async() => {
-    let signedGetUrl
 
     beforeAll(async () => {
+      // signed GET object URL from aws-sdk-js
       const command = new GetObjectCommand({
           Bucket: BUCKET_NAME,
           Key: OBJECT_KEY,
       });
+      signedGetUrlFromSdk = await getSignedUrl(client, command, { uriEscapePath: true, expiresIn: EXPIRATION_TIME });
 
-      signedGetUrl = await getSignedUrl(client, command, { expiresIn: EXPIRATION_TIME });
+      // signed GET object URL from awscli
+      const args = [
+        `s3://${ BUCKET_NAME }/${ OBJECT_KEY }`,
+        `--profile ${ AWS_PROFILE }`,
+        `--expires-in ${ EXPIRATION_TIME }`,
+      ]
+      signedGetUrlFromCli = await exec(`aws s3 presign ${ args.join(' ') }`)
+      signedGetUrlFromCli = signedGetUrlFromCli.stdout.replace(/\n/g, '');
     })
     test(`GET URL was generated`, () => {
-      expect(signedGetUrl).toBeDefined()
+      expect(signedGetUrlFromSdk).toBeDefined()
     })
-    test(`GET URL returns 200`, async () => {
-      const objectDownloadResponse = await fetch(signedGetUrl)
+    test(`GET URL from AWS-CLI returns 200`, async () => {
+      const objectDownloadResponse = await fetch(signedGetUrlFromCli)
+      expect(objectDownloadResponse.ok).toBeTrue()
+    })
+    test(`GET URL from AWS-SDK-JS returns 200`, async () => {
+      const objectDownloadResponse = await fetch(signedGetUrlFromSdk)
       expect(objectDownloadResponse.ok).toBeTrue()
     })
   })
