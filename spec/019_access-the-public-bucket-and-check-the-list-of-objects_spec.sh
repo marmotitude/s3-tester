1badd626823bb75c3fc311d8d765ef3bbdef4f5a
Fabricio C Zuardi <fabricio@fabricio.org>
2024-06-12 09:07:08 -0300

    refactor wait_command function and add support for object-exists

--- spec/019_access-the-public-bucket-and-check-the-list-of-objects_spec.sh
+++ spec/019_access-the-public-bucket-and-check-the-list-of-objects_spec.sh
@@ -1,18 +1,20 @@
 wait_command() {
-  profile_to_wait=$1
-  bucket_name_to_wait=$2
-  number_of_waits=$3
-  command=$4
+  command=$1
+  profile_to_wait=$2
+  bucket_name_to_wait=$3
+  object_name_to_wait=$4
+  key_argument=$([ -z "$4" ] && echo "" || echo "--key $4")
+  number_of_waits=$NUMBER_OF_WAITS
 
   # aws s3api wait does not allow a custom timeout, so we repeat several waits if we need more than
   # the default of 20 attempts, or 100 seconds
   for ((i=1; i<=number_of_waits; i++))
   do
     echo "wait $command for profile $profile_to_wait attempt number: $i, $(date)"
-    aws --profile $profile_to_wait s3api wait $command --bucket $bucket_name_to_wait 2>&1 || echo falhou $i
+    aws --profile $profile_to_wait s3api wait $command --bucket $bucket_name_to_wait $key_argument 2>&1 || echo falhou $i
   done
   echo ".last wait $command for profile $profile_to_wait, $(date)"
-  aws --profile $profile_to_wait s3api wait $command --bucket $bucket_name_to_wait
+  aws --profile $profile_to_wait s3api wait $command --bucket $bucket_name_to_wait $key_argument
 }
 
 Describe 'Access the public bucket and check the list of objects:' category:"Bucket Permission"
@@ -29,12 +31,11 @@ Describe 'Access the public bucket and check the list of objects:' category:"Buc
     profile=$1
     client=$2
     aws --profile $profile s3api create-bucket --bucket $bucket_name-$client --acl public-read > /dev/null
-    wait_command $profile "$bucket_name-$client" $NUMBER_OF_WAITS bucket-exists
-    #aws --profile $profile s3api wait bucket-exists --bucket $bucket_name-$client
-    wait_command "$profile-second" "$bucket_name-$client" $NUMBER_OF_WAITS bucket-exists
-    #aws --profile $profile-second s3api wait bucket-exists --bucket $bucket_name-$client
+    wait_command bucket-exists $profile "$bucket_name-$client"
+    wait_command  bucket-exists "$profile-second" "$bucket_name-$client"
     aws --profile $profile s3 cp $file1_name s3://$bucket_name-$client > /dev/null
     aws --profile $profile s3api wait object-exists --bucket $bucket_name-$client --key $file1_name
+    wait_command object-exists $profile "$bucket_name-$client" "$file1_name"
     case "$client" in
     "aws-s3api" | "aws" | "aws-s3")
       When run aws --profile $profile-second s3api list-objects-v2 --bucket $bucket_name-$client
@@ -52,6 +53,6 @@ Describe 'Access the public bucket and check the list of objects:' category:"Buc
     esac
     The status should be success
     rclone purge --log-file /dev/null "$profile:$bucket_name-$client" > /dev/null
-    wait_command $profile "$bucket_name-$client" $NUMBER_OF_WAITS bucket-not-exists
+    wait_command bucket-not-exists $profile "$bucket_name-$client"
   End
 End
