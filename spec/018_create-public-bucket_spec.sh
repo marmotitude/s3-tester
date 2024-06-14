649958445cffd35abf3888f08b2b25400cca23b3
Bruno Fernandes <bruno.fernandes1996@hotmail.com>
2024-06-14 09:19:06 -0300

    fix: adicionado waiter mais robusto

--- spec/018_create-public-bucket_spec.sh
+++ spec/018_create-public-bucket_spec.sh
@@ -1,3 +1,5 @@
+Include ./spec/019_utils.sh
+
 Describe 'Create public bucket:' category:"Bucket Permission"
   setup(){
     bucket_name="test-018-$(date +%s)"
@@ -29,7 +31,7 @@ Describe 'Create public bucket:' category:"Bucket Permission"
     esac
     The status should be success
     aws --profile $profile s3api wait bucket-exists --bucket $bucket_name-$client
-    aws s3 rb s3://$bucket_name-$client --profile $profile --force > /dev/null
-    aws s3api wait bucket-not-exists --bucket $bucket_name-$client --profile $profile
+    rclone purge --log-file /dev/null "$profile:$bucket_name-$client" > /dev/null
+    wait_command bucket-not-exists $profile "$bucket_name-$client"
   End
 End
