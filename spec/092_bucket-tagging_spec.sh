a10be8e3cd00b0a7b1f304b3ca67d0a17fd2acf0
Bruno Fernandes <bruno.fernandes1996@hotmail.com>
2024-07-30 15:55:20 -0300

    tagging tests

---
+++ spec/092_bucket-tagging_spec.sh
@@ -0,0 +1,111 @@
+# import functions: wait_command
+Include ./spec/019_utils.sh
+is_variable_null() {
+  [ -z "$1" ]
+}
+
+Describe 'Put bucket tagging:' category:"Bucket Management"
+  setup(){
+    bucket_name="test-092-$(date +%s)"
+    file1_name="LICENSE"
+  }
+  Before 'setup' 
+  Parameters:matrix
+    $PROFILES
+    $CLIENTS
+  End
+  Example "on profile $1 using client $2" id:"092"
+    profile=$1
+    client=$2
+    #policy vars
+    tag='TagSet=[{Key=organization,Value=marketing}]'
+    aws --profile $profile s3 mb s3://$bucket_name-$client > /dev/null 
+    case "$client" in
+    "aws-s3api" | "aws" | "aws-s3")
+      When run aws --profile $profile s3api put-bucket-tagging --bucket $bucket_name-$client --tagging $tag
+      The stdout should include ""
+      The status should be success
+      ;;
+    "rclone")
+      Skip "Skipped test to $client"
+      ;;
+    "mgc")
+      Skip "Skipped test to $client"
+      ;;
+    esac
+    #wait_command bucket-exists "$profile" "$bucket_name-$client"
+    rclone purge $profile:$bucket_name-$client > /dev/null
+    #wait_command bucket-not-exists "$profile" "$bucket_name-$client"
+  End
+End
+
+Describe 'Get bucket tagging:' category:"Bucket Management"
+  setup(){
+    bucket_name="test-092-$(date +%s)"
+    file1_name="LICENSE"
+  }
+  Before 'setup' 
+  Parameters:matrix
+    $PROFILES
+    $CLIENTS
+  End
+  Example "on profile $1 using client $2" id:"092"
+    profile=$1
+    client=$2
+    #policy vars
+    tag='TagSet=[{Key=organization,Value=marketing}]'
+    aws --profile $profile s3 mb s3://$bucket_name-$client > /dev/null 
+    aws --profile $profile s3api put-bucket-tagging --bucket $bucket_name-$client --tagging $tag
+    case "$client" in
+    "aws-s3api" | "aws" | "aws-s3")
+      When run aws --profile $profile s3api get-bucket-tagging --bucket $bucket_name-$client
+      The stdout should include "organization"
+      The status should be success
+      ;;
+    "rclone")
+      Skip "Skipped test to $client"
+      ;;
+    "mgc")
+      Skip "Skipped test to $client"
+      ;;
+    esac
+    #wait_command bucket-exists "$profile" "$bucket_name-$client"
+    rclone purge $profile:$bucket_name-$client > /dev/null
+    #wait_command bucket-not-exists "$profile" "$bucket_name-$client"
+  End
+End
+
+Describe 'Delete bucket tagging:' category:"Bucket Management"
+  setup(){
+    bucket_name="test-092-$(date +%s)"
+    file1_name="LICENSE"
+  }
+  Before 'setup' 
+  Parameters:matrix
+    $PROFILES
+    $CLIENTS
+  End
+  Example "on profile $1 using client $2" id:"092"
+    profile=$1
+    client=$2
+    #policy vars
+    tag='TagSet=[{Key=organization,Value=marketing}]'
+    aws --profile $profile s3 mb s3://$bucket_name-$client > /dev/null 
+    case "$client" in
+    "aws-s3api" | "aws" | "aws-s3")
+      When run aws --profile $profile s3api delete-bucket-tagging --bucket $bucket_name-$client
+      The stdout should include ""
+      The status should be success
+      ;;
+    "rclone")
+      Skip "Skipped test to $client"
+      ;;
+    "mgc")
+      Skip "Skipped test to $client"
+      ;;
+    esac
+    #wait_command bucket-exists "$profile" "$bucket_name-$client"
+    rclone purge $profile:$bucket_name-$client > /dev/null
+    #wait_command bucket-not-exists "$profile" "$bucket_name-$client"
+  End
+End
