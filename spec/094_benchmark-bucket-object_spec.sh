61d48f69ab22dea52dbc3a2a7c87c96e5dc121b8
Bruno Fernandes <bruno.fernandes1996@hotmail.com>
2024-08-05 17:48:17 -0300

    fix print on actions and more tests

--- spec/094_benchmark-bucket-object_spec.sh
+++ spec/094_benchmark-bucket-object_spec.sh
@@ -35,7 +35,7 @@ Describe 'Verificar a existência do bucket'
     wait_command bucket-exists "$profile" "$bucket_name-$client"
     end_time=$(date +%s)
     bucket_exists_time=$((end_time - start_time))
-    echo "Tempo para verificar existência do bucket no perfil $profile: $bucket_exists_time" >> ./report/benchmark-buckets.txt
+    echo "Tempo para verificar a existência do bucket no perfil $profile: $bucket_exists_time" >> ./report/benchmark-buckets.txt
     rclone purge $profile:$bucket_name-$client > /dev/null
   End
 End
@@ -77,7 +77,7 @@ Describe 'Verificar a existência do bucket publico'
   Example "bucket exists on profile $1-second using client $2" id:"094"
     profile=$1
     client=$2
-    aws --profile $profile s3 mb s3://$bucket_name-$client > /dev/null
+    aws --profile $profile s3api create-bucket --bucket $bucket_name-$client --acl public-read > /dev/null
     start_time=$(date +%s)
     wait_command bucket-exists "$profile-second" "$bucket_name-$client"
     end_time=$(date +%s)
@@ -101,7 +101,7 @@ Describe 'Verificar a existência do objeto publico'
     profile=$1
     client=$2
     aws --profile $profile s3 mb s3://$bucket_name-$client > /dev/null
-    aws --profile $profile s3 cp $file1_name s3://$bucket_name-$client --acl public-read> /dev/null
+    aws --profile $profile s3 cp $file1_name s3://$bucket_name-$client --acl public-read > /dev/null
     start_time=$(date +%s)
     wait_command object-exists "$profile-second" "$bucket_name-$client" "$file1_name"
     end_time=$(date +%s)
@@ -109,4 +109,106 @@ Describe 'Verificar a existência do objeto publico'
     echo "Tempo para verificar a existência do objeto publico no perfil $profile-second: $object_exists_time" >> ./report/benchmark-buckets.txt
     rclone purge $profile:$bucket_name-$client > /dev/null
   End
+End
+
+############
+
+Describe 'Verificar a inexistência do bucket vazio após deletar'
+  setup(){
+    bucket_name="test-094-$(date +%s)"
+    file1_name="LICENSE"
+  }
+  Before 'setup'
+  Parameters:matrix
+    $PROFILES
+    $CLIENTS
+  End
+  Example "bucket exists on profile $1 using client $2" id:"094"
+    profile=$1
+    client=$2
+    aws --profile $profile s3 mb s3://$bucket_name-$client > /dev/null
+    aws --profile $profile s3 rb s3://$bucket_name-$client > /dev/null
+    start_time=$(date +%s)
+    wait_command bucket-not-exists "$profile" "$bucket_name-$client"
+    end_time=$(date +%s)
+    bucket_exists_time=$((end_time - start_time))
+    echo "Tempo para verificar a inexistência do bucket vazio deletado no perfil $profile: $bucket_exists_time" >> ./report/benchmark-buckets.txt
+    End
+End
+
+Describe 'Verificar a inexistência do objeto deletado'
+  setup(){
+    bucket_name="test-094-$(date +%s)"
+    file1_name="LICENSE"
+  }
+  Before 'setup'
+  Parameters:matrix
+    $PROFILES
+    $CLIENTS
+  End
+  Example "bucket exists on profile $1 using client $2" id:"094"
+    profile=$1
+    client=$2
+    aws --profile $profile s3 mb s3://$bucket_name-$client > /dev/null
+    aws --profile $profile s3 cp $file1_name s3://$bucket_name-$client > /dev/null
+    wait_command object-exists "$profile" "$bucket_name-$client" "$file1_name"
+    aws --profile $profile s3 rm s3://$bucket_name-$client/$file1_name > /dev/null
+    start_time=$(date +%s)
+    wait_command object-not-exists "$profile" "$bucket_name-$client" "$file1_name"
+    end_time=$(date +%s)
+    object_exists_time=$((end_time - start_time))
+    echo "Tempo para verificar a inexistência do objeto deletado no perfil $profile: $object_exists_time" >> ./report/benchmark-buckets.txt
+    rclone purge $profile:$bucket_name-$client > /dev/null
+  End
+End
+
+Describe 'Verificar a inexistência do bucket publico deletado'
+  setup(){
+    bucket_name="test-094-$(date +%s)"
+    file1_name="LICENSE"
+  }
+  Before 'setup'
+  Parameters:matrix
+    $PROFILES
+    $CLIENTS
+  End
+  Example "bucket exists on profile $1-second using client $2" id:"094"
+    profile=$1
+    client=$2
+    aws --profile $profile s3api create-bucket --bucket $bucket_name-$client --acl public-read > /dev/null
+    wait_command bucket-exists "$profile-second" "$bucket_name-$client"
+    aws --profile $profile s3 rb s3://$bucket_name-$client  > /dev/null
+    start_time=$(date +%s)
+    wait_command bucket-not-exists "$profile-second" "$bucket_name-$client"
+    end_time=$(date +%s)
+    bucket_exists_time=$((end_time - start_time))
+    echo "Tempo para verificar a inexistência do bucket publico deletado no perfil $profile-second: $bucket_exists_time" >> ./report/benchmark-buckets.txt
+  End
+End
+
+Describe 'Verificar a inexistência do objeto publico deletado'
+  setup(){
+    bucket_name="test-094-$(date +%s)"
+    file1_name="LICENSE"
+  }
+  Before 'setup'
+  Parameters:matrix
+    $PROFILES
+    $CLIENTS
+  End
+  Example "bucket exists on profile $1-second using client $2" id:"094"
+    profile=$1
+    client=$2
+    aws --profile $profile s3 mb s3://$bucket_name-$client > /dev/null
+    aws --profile $profile s3 cp $file1_name s3://$bucket_name-$client --acl public-read > /dev/null
+    wait_command object-exists "$profile-second" "$bucket_name-$client" "$file1_name"
+    aws --profile $profile s3 rm s3://$bucket_name-$client/$file1_name > /dev/null
+    start_time=$(date +%s)
+    wait_command object-not-exists "$profile-second" "$bucket_name-$client" "$file1_name"
+    end_time=$(date +%s)
+    object_exists_time=$((end_time - start_time))
+    echo "Tempo para verificar a inexistência do objeto publico deletado no perfil $profile-second: $object_exists_time" >> ./report/benchmark-buckets.txt
+    rclone purge $profile:$bucket_name-$client > /dev/null
+  End
+  cat ./report/benchmark-buckets.txt
 End
