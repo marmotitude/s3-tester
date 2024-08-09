5580c49da773d44de32c815714e386c3d0ece811
Bruno Fernandes <54373093+folkzb@users.noreply.github.com>
2024-08-09 14:12:05 -0300

    Update 025

--- spec/025_access-the-private-with-acl-bucket-and-check-the-access-of-objects_spec.sh
+++ spec/025_access-the-private-with-acl-bucket-and-check-the-access-of-objects_spec.sh
@@ -42,7 +42,7 @@ Describe 'Access the Private with ACL bucket and check the access of objects:' c
       The stderr should include "403"
       ;;
     esac
-    aws s3 rb s3://$bucket_name-$client --profile $profile --force > /dev/null
+    rclone purge --log-file /dev/null "$profile:$bucket_name-$client" > /dev/null
     aws s3api wait bucket-not-exists --bucket $bucket_name-$client --profile $profile
   End
 End
