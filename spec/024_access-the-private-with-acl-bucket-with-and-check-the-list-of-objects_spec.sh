dfc858cae1d2527f7e52d0b53097c5c01ef41ea2
Bruno Fernandes <54373093+folkzb@users.noreply.github.com>
2024-08-14 10:02:11 -0300

    Update 024 remove file1_name

--- spec/024_access-the-private-with-acl-bucket-with-and-check-the-list-of-objects_spec.sh
+++ spec/024_access-the-private-with-acl-bucket-with-and-check-the-list-of-objects_spec.sh
@@ -27,19 +27,19 @@ Describe 'Access the Private with ACL bucket with and check the list of objects:
     aws s3api --profile $profile put-bucket-acl --bucket $bucket_name-$client --grant-read id=$id > /dev/null
     case "$client" in
     "aws-s3api" | "aws" | "aws-s3")
-    wait_command bucket-exists $profile "$bucket_name-$client" "$file1_name"
+    wait_command bucket-exists $profile "$bucket_name-$client" 
     #aws --profile $profile-second s3api wait bucket-exists --bucket $bucket_name-$client
     When run aws --profile $profile-second s3api list-objects-v2 --bucket $bucket_name-$client
     The output should include "$file1_name"
       ;;
     "rclone")
-    wait_command bucket-exists $profile "$bucket_name-$client" "$file1_name"
+    wait_command bucket-exists $profile "$bucket_name-$client""
     #aws --profile $profile-second s3api wait bucket-exists --bucket $bucket_name-$client
     When run rclone ls $profile-second:$bucket_name-$client
     The output should include "$file1_name"
       ;;
     "mgc")
-      wait_command bucket-exists $profile "$bucket_name-$client" "$file1_name"
+      wait_command bucket-exists $profile "$bucket_name-$client"
       #aws --profile $profile-second s3api wait bucket-exists --bucket $bucket_name-$client
       mgc profile set $profile-second > /dev/null
       When run mgc object-storage objects list --dst $bucket_name-$client --raw
