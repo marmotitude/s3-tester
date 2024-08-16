4362aba62ce2ce2da9355ad524ead0f1dd0d2464
Bruno Fernandes <54373093+folkzb@users.noreply.github.com>
2024-08-16 12:04:54 -0300

    Update 022_access-the-private-bucket-and-check-the-list-of-objects_spec.sh

--- spec/022_access-the-private-bucket-and-check-the-list-of-objects_spec.sh
+++ spec/022_access-the-private-bucket-and-check-the-list-of-objects_spec.sh
@@ -26,10 +26,10 @@ Describe 'Access the private bucket and check the list of objects:' category:"Bu
     ;;
     "mgc")
       mgc profile set $profile-second > /dev/null
-      When run bash ./spec/retry_command.sh "mgc object-storage objects list --dst $bucket_name-$client --raw"
-      #When run mgc object-storage objects list --dst $bucket_name-$client --raw
-      The output should include "403"
-      #The stderr should include "403"
+      #When run bash ./spec/retry_command.sh "mgc object-storage objects list --dst $bucket_name-$client --raw"
+      When run mgc object-storage objects list --dst $bucket_name-$client --raw
+      #The output should include "403"
+      The stderr should include "403"
     ;;
     esac
     The status should be failure
