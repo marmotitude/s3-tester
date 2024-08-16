57d6cfbf9a818889b125e01b2372b33dacd0819f
Bruno Fernandes <54373093+folkzb@users.noreply.github.com>
2024-08-16 15:40:12 -0300

    Update retry_command.sh
    definindo tempo default para retry command

--- spec/retry_command.sh
+++ spec/retry_command.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 command=$1
-timeout_duration=240 # Tempo máximo de execução em segundos (240 = 4 minutos)
+timeout_duration=${2:-240} # Tempo máximo de execução em segundos (240 = 4 minutos)
 
 end_time=$((SECONDS + timeout_duration))
 
