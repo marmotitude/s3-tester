dffadbb711ce483db2846aac6bea8e93380192bb
Bruno Fernandes <bruno.fernandes1996@hotmail.com>
2024-08-16 10:09:57 -0300

    feature on retry

--- spec/retry_command.sh
+++ spec/retry_command.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 command=$1
+timeout_duration=10 # Tempo máximo de execução em segundos (240 = 4 minutos)
 
 while true; do
-  output=$($command 2>&1)
+  output=$(timeout $timeout_duration $command 2>&1)
   status=$?
 
   if [ $status -eq 0 ]; then
     echo "$output"
     exit 0
+  fi
+
+  if [ $status -eq 124 ]; then
+    echo "Timeout de $timeout_duration segundos alcançado. O comando $command não foi concluído dentro de $timeout_duration segundos."
+    exit 1
   fi
 
   if echo "$output" | grep -q "connection reset by peer"; then
@@ -32,4 +38,3 @@ while true; do
   echo "Unexpected error: $output"
   exit 1
 done
-
