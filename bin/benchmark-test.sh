12d777bcbebe68efbe77b7146234f6fdd2a87ee9
Bruno Fernandes <bruno.fernandes1996@hotmail.com>
2024-08-22 10:39:02 -0300

    feat ci

---
+++ bin/benchmark-test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+SCRIPT_PATH="$( cd "$( echo "${BASH_SOURCE[0]%/*}" )" && pwd )"
+
+# split arguments before and after double dash
+source "$SCRIPT_PATH/lib/doubledashsplit.sh"
+
+# arguments
+source "$SCRIPT_PATH/../vendor/yaacov/argparse.sh"
+define_arg "clients" "aws-s3api,aws-s3,rclone,mgc" "S3 clients that will perform the tests" "string" "false"
+define_arg "profiles" "" "Profiles to use in the tests, must have the same name on all clients" "string" "true"
+define_arg "tests" "" "A list of individual tests to perform" "string" "false"
+define_arg "sizes" "" "A list of individual sizes on benchmark to perform" "string" "false"
+define_arg "quantity" "" "A list of individual quantity on benchmark to perform" "string" "false"
+set_description "Run tests on multiple S3 providers using multiple S3 clients.\nUse -- [shellspec args...] in the end to pass more arguments to shellspec."
+
+check_for_help $args_after_double_dash
+parse_args $args_before_double_dash
+
+# convert comma separated lists to space separated
+clients="${clients//,/ }"
+profiles="${profiles//,/ }"
+tests="${tests//,/ }"
+sizes="${sizes//,/ }"
+quantity="${quantity//,/ }"
+
+# convert test numbers to test "id" tags
+tag_args=""
+for num in $tests; do
+    padded_num=$(printf "%03d" $num)  # Padding each number with zeroes to three digits
+    tag_args+=" --tag id:${padded_num}"
+done
+
+# print the tools shasum
+shasum `which mgc` `which aws` `which rclone`
+
+# run the tests
+shellspec -c "$SCRIPT_PATH/../spec" --env CLIENTS="$clients" --env PROFILES="$profiles" --env SIZES="$sizes" --env QUANTITY="$quantity" -s bash $tag_args $args_after_double_dash
--- spec/100_benchmark-test_spec.sh
+++ spec/100_benchmark-test_spec.sh
@@ -11,81 +11,82 @@ Describe 'Benchmark test:' category:"Bucket Management"
   setup() {
     bucket_name="test-100-$(date +%s)"
     date=$(date +%s.%N)
-    sizes="1,2" #param
-    quantity="10,100" #param
   }
 
   Before 'setup'
     Parameters:matrix
       $PROFILES
       $CLIENTS
+      $SIZES
+      $QUANTITY
     End
 
   Example "on profile $1 using client $2" id:"100"
     profile=$1
     client=$2
-    #echo "date,profile,client,operation,size,time" >> ./report/benchmark.csv
+    sizes=$3 #param
+    quantity=$4 #param
     aws --profile "$profile" s3 mb s3://"$bucket_name-$client" > /dev/null
     for size in $(echo $sizes | tr "," "\n")
     do
       for i in $(seq 1 $quantity); do
         fallocate -l "${size}M" "./report/arquivo_${size}M_$i.txt"
       done
-      #echo que mande tudo para a mesma linha o case
+      echo $date,$profile,$client,$size>> ./report/benchmark.csv
       case "$client" in
         "aws-s3api" | "aws" | "aws-s3")
           for i in $(seq 1 $quantity); do
-            create_time=$(measure_time aws --profile $profile s3 cp ./report/arquivo_$i.txt s3://$bucket_name-$client)
-            echo "$date,$profile,aws,create$i,$size,$create_time" >> ./report/benchmark.csv
+            time=$(measure_time aws --profile $profile s3 cp ./report/arquivo_$i.txt s3://$bucket_name-$client)
+            printf ",create%d,%s" "$i" "$time" >> ./report/benchmark.csv
           done
           for i in $(seq 1 $quantity); do
-            download_time=$(measure_time aws --profile $profile s3 cp s3://$bucket_name-$client/arquivo_$i.txt ./$bucket_name-$client/arquivo_$i.txt)
-            echo "$date,$profile,aws,download$i,$size,$download_time" >> ./report/benchmark.csv
+            time=$(measure_time aws --profile $profile s3 cp s3://$bucket_name-$client/arquivo_$i.txt ./$bucket_name-$client/arquivo_$i.txt)
+            printf ",download%d,%s" "$i" "$time" >> ./report/benchmark.csv
           done
           for i in $(seq 1 $quantity); do
-            update_time=$(measure_time aws --profile $profile s3 cp ./report/arquivo_$i.txt s3://$bucket_name-$client)
-            echo "$date,$profile,aws,update$i,$size,$update_time" >> ./report/benchmark.csv
+            time=$(measure_time aws --profile $profile s3 cp ./report/arquivo_$i.txt s3://$bucket_name-$client)
+            printf ",update%d,%s,%s" "$i" "$time" >> ./report/benchmark.csv
           done
           for i in $(seq 1 $quantity); do
-            delete_time=$(measure_time aws --profile $profile s3 rm s3://$bucket_name-$client/arquivo_$i.txt)
-            echo "$date,$profile,aws,delete$i,$size,$delete_time" >> ./report/benchmark.csv
+            time=$(measure_time aws --profile $profile s3 rm s3://$bucket_name-$client/arquivo_$i.txt)
+            printf ",delete%d,%s" "$i" "$time" >> ./report/benchmark.csv
           done
           ;;
         "rclone")
           for i in $(seq 1 $quantity); do
             create_time=$(measure_time rclone s3 copy ./report/arquivo_$i.txt $profile:$bucket_name-$client)
-            echo "$date,$profile,rclone,create$i,$size,$create_time" >> ./report/benchmark.csv
+            printf ",create%d,%s" "$i" "$time" >> ./report/benchmark.csv
           done
           for i in $(seq 1 $quantity); do
             download_time=$(measure_time rclone copy $profile:$bucket_name-$client/arquivo_$i.txt ./$bucket_name-$client/arquivo_$i.txt)
-            echo "$date,$profile,rclone,download$i,$size,$download_time" >> ./report/benchmark.csv
+            printf ",download%d,%s" "$i" "$time" >> ./report/benchmark.csv
           done
           for i in $(seq 1 $quantity); do
             update_time=$(measure_time rclone copy ./report/arquivo_$i.txt $profile:$bucket_name-$client)
-            echo "$date,$profile,rclone,update$i,$size,$update_time" >> ./report/benchmark.csv
+            printf ",update%d,%s,%s" "$i" "$time" >> ./report/benchmark.csv
           done
           for i in $(seq 1 $quantity); do
             delete_time=$(measure_time rclone delete $profile:$bucket_name-$client/arquivo_$i.txt)
-            echo "$date,$profile,rclone,delete$i,$size,$delete_time" >> ./report/benchmark.csv
+            printf ",delete%d,%s" "$i" "$time" >> ./report/benchmark.csv
           done
           ;;
         "mgc")
           mgc profile set "$profile" > /dev/null
           for i in $(seq 1 $quantity); do
             create_time=$(measure_time mgc object-storage objects upload ./report/arquivo_$i.txt $bucket_name-$client)
-            echo "$date,$profile,mgc,create$i,$size,$create_time" >> ./report/benchmark.csv
+            printf ",create%d,%s" "$i" "$time" >> ./report/benchmark.csv
           done
           for i in $(seq 1 $quantity); do
             download_time=$(measure_time mgc object-storage objects download $bucket_name-$client/arquivo_$i.txt ./$bucket_name-$client/arquivo_$i.txt)
-            echo "$date,$profile,mgc,download$i,$size,$download_time" >> ./report/benchmark.csv
+            printf ",download%d,%s" "$i" "$time" >> ./report/benchmark.csv
           done
           for i in $(seq 1 $quantity); do
             update_time=$(measure_time mgc object-storage objects upload ./report/arquivo_$i.txt $bucket_name-$client)
-            echo "$date,$profile,mgc,update$i,$size,$update_time" >> ./report/benchmark.csv
+            printf ",update%d,%s,%s" "$i" "$time" >> ./report/benchmark.csv
           done
           for i in $(seq 1 $quantity); do
             delete_time=$(measure_time mgc object-storage objects delete $bucket_name-$client/arquivo_$i.txt --no-confirm)
-            echo "$date,$profile,mgc,delete$i,$size,$delete_time" >> ./report/benchmark.csv
+            printf ",delete%d,%s" "$i" "$time" >> ./report/benchmark.csv
           done
           ;;
       esac
