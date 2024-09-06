35f32dc2ff81cace0b4f3a52446e224d00bb1536
Bruno Fernandes <54373093+folkzb@users.noreply.github.com>
2024-09-06 09:19:59 -0300

    Update 100_benchmark-test_spec.sh

--- spec/100_benchmark-test_spec.sh
+++ spec/100_benchmark-test_spec.sh
@@ -106,14 +106,12 @@ Describe 'Benchmark test:' category:"Bucket Management"
       esac
     done
     rclone purge $profile:$bucket_name-$client > /dev/null
-    #aws s3 --profile br-se1 cp ./report/benchmark.csv s3://benchmark/data/${date}h.csv --acl public-read > /dev/null
     python3 ./bin/process_data.py
-
-
-    #aws s3 --profile br-se1 cp ./report/${date}h-processed_data.csv s3://benchmark/processed_data/${date}h.csv --acl public-read > /dev/null
     python3 ./bin/benchmark.py
-    #aws s3 --profile br-se1 cp ./report/${date}h-dashboard.html s3://benchmark/dashboards/${date}h-dashboard.html --acl public-read > /dev/null
-    #aws s3 --profile br-se1 cp ./report/${date}h-dashboard.html s3://benchmark/dashboards/index.html --acl public-read > /dev/null
+    aws s3 --profile br-se1 cp ./report/benchmark.csv s3://benchmark/data/${date}h.csv --acl public-read > /dev/null
+    aws s3 --profile br-se1 cp ./report/${date}h-processed_data.csv s3://benchmark/processed_data/${date}h.csv --acl public-read > /dev/null
+    aws s3 --profile br-se1 cp ./report/${date}h-dashboard.html s3://benchmark/dashboards/${date}h-dashboard.html --acl public-read > /dev/null
+    aws s3 --profile br-se1 cp ./report/${date}h-dashboard.html s3://benchmark/dashboards/index.html --acl public-read > /dev/null
 
 End
 
