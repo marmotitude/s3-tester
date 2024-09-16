8802e42bfda3e89389be7e7cbf01f312c0a1f9dc
Bruno Fernandes <54373093+folkzb@users.noreply.github.com>
2024-09-16 17:27:16 -0300

    Update 052_delete-versions-in-batch_spec.sh

--- spec/052_delete-versions-in-batch_spec.sh
+++ spec/052_delete-versions-in-batch_spec.sh
@@ -15,6 +15,7 @@ Describe 'Delete versions in batch:' category:"Object Versioning"
     aws s3api --profile $profile put-bucket-versioning --bucket $bucket_name-$client --versioning-configuration Status=Enabled > /dev/null
     aws --profile $profile s3 cp $file1_name  s3://$bucket_name-$client > /dev/null
     aws --profile $profile s3 cp $file1_name  s3://$bucket_name-$client > /dev/null
+    sleep 10
     case "$client" in
     "aws-s3api" | "aws" | "aws-s3")
     When run aws --profile $profile s3api delete-objects --bucket $bucket_name-$client --delete "$(aws --profile $profile s3api list-object-versions --bucket $bucket_name-$client| jq '{Objects: [.Versions[] | {Key:.Key, VersionId : .VersionId}], Quiet: false}')"
