899cdbdc67fc3048498efc804b61dd85568f2c0a
Fabricio C Zuardi <fabricio@fabricio.org>
2024-11-27 07:05:44 -0300

    add support for categories and tags

--- bin/test.sh
+++ bin/test.sh
@@ -9,6 +9,8 @@ source "$SCRIPT_PATH/../vendor/yaacov/argparse.sh"
 define_arg "clients" "aws-s3api,aws-s3,rclone,mgc" "S3 clients that will perform the tests" "string" "false"
 define_arg "profiles" "" "Profiles to use in the tests, must have the same name on all clients" "string" "true"
 define_arg "tests" "" "A list of individual tests to perform" "string" "false"
+define_arg "categories" "" "(optional) A list of test groups to include" "string" "false"
+define_arg "tags" "" "(optional) A list of shellspec tags to include" "string" "false"
 set_description "Run tests on multiple S3 providers using multiple S3 clients.\nUse -- [shellspec args...] in the end to pass more arguments to shellspec."
 
 check_for_help $args_after_double_dash
@@ -20,6 +22,8 @@ profiles_to_clean=$profiles
 clients="${clients//,/ }"
 profiles="${profiles//,/ }"
 tests="${tests//,/ }"
+categories="${categories//,/ }"
+tags="${categories//,/ }"
 
 # convert test numbers to test "id" tags
 tag_args=""
@@ -27,6 +31,13 @@ for num in $tests; do
     padded_num=$(printf "%03d" $num)  # Padding each number with zeroes to three digits
     tag_args+=" --tag id:${padded_num}"
 done
+for category in $categories; do
+    tag_args+=" --tag category:${category}"
+done
+for tag in $tags; do
+    tag_args+=" --tag ${tag}"
+done
+echo "$tag_args"
 
 # print the tools shasum
 shasum `which mgc` `which aws` `which rclone`
