e173562d8f504a556e6a699990d515cdb2092900
Bruno Fernandes <54373093+folkzb@users.noreply.github.com>
2024-12-10 11:29:27 -0300

    fix 36,38 curl command (#247)

--- spec/036_validate-the-url-of-presigned_spec.sh
+++ spec/036_validate-the-url-of-presigned_spec.sh
@@ -27,7 +27,8 @@ Describe 'get-presign:' category:"BucketSharing"
       ;;
     "mgc")
     mgc workspace set $profile > /dev/null
-    presign_url=$(mgc object-storage objects presign --dst $test_bucket_name/$file1_name --expires-in "5m")
+    presign_url=$(mgc object-storage objects presign --dst $test_bucket_name/$file1_name)
+    presign_url=$(echo "$presign_url" | grep -oP 'https.*?host')
     When run curl $presign_url
     The output should include Copyright
     The error should include Current
@@ -64,7 +65,8 @@ Describe 'put-presign:' category:"BucketSharing"
       ;;
     "mgc")
     mgc workspace set $profile > /dev/null
-    presign_url=$(mgc object-storage objects presign --dst $test_bucket_name/$file1_name --expires-in "5m" --method PUT); presign_url="https${presign_url#*https}"
+    presign_url=$(mgc object-storage objects presign --dst $test_bucket_name/$file1_name --expires-in "5m" --method PUT)
+    presign_url=$(echo "$presign_url" | grep -oP 'https.*?host')
     When run curl -X PUT -T $file1_name $presign_url
     The error should include Current
       ;;
